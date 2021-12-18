b25818e85a16b2e84a7eef615b7ec2aadaa87408
kdd9512 <hyunwoo8805@gmail.com>
2021-12-18 20:26:44 +0900

    20211218 1st commit
    react challenge finale
    clone netflix
    
    react challenge 중
    
    초기세팅.
    rest global style 출처 표기.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createGlobalStyle, ThemeProvider} from "styled-components";
+import {RecoilRoot} from "recoil";
+import {theme} from "./theme";
+
+// 출처 : https://meyerweb.com/eric/tools/css/reset/
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200;400&display=swap');
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  body {
+    line-height: 1;
+  }
+
+  ol, ul {
+    list-style: none;
+  }
+
+  blockquote, q {
+    quotes: none;
+  }
+
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  // 만약 상단에서 @import 가 실패하면 이하의 글꼴을 적용한다.
+  body {
+    font-weight: 300;
+    font-family: 'Source Sans Pro', sans-serif;
+    color: black;
+    line-height: 1.2;
+  }
+  
+    // Link 태그는 결국 HTML 에서 a 태그로 바뀌므로 이곳에서 CSS 를 설정한다.
+    a {
+      text-decoration: none;
+      color: inherit;
+    }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+            <App/>
+        </ThemeProvider>
+    </RecoilRoot>
+    , document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
