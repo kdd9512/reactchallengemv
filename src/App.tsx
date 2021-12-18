dbdc97f1d90342e9f2a5840698a54b3e114c90df
kdd9512 <hyunwoo8805@gmail.com>
2021-12-19 00:31:08 +0900

    20211218 1st commit
    react challenge finale
    clone netflix
    
    react challenge 중
    
    header 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Routes/Home";
+import Tv from "./Routes/TV";
+import Search from "./Routes/Search";
+import Header from "./Components/Header";
 
 function App() {
-  return (
-    <></>
-  );
+    return (
+        <React.Fragment>
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/tv" element={<Tv/>}/>
+                    <Route path="/search" element={<Search/>}/>
+                </Routes>
+            </BrowserRouter>
+        </React.Fragment>
+    );
 }
 
 export default App;
